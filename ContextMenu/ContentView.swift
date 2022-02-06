811937ec572b0ebb3e3e5a9417e8409b0558d1ff
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2022-02-06 06:11:12 -0800

    Initial Commit

---
+++ ContextMenu/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ContextMenu
+//
+//  Created by Ierchenko Anna  on 2/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       Image("LA")
+        .resizable()
+        .frame(height: 300)
+        .cornerRadius(30)
+        .padding()
+        .contextMenu {
+            VStack {
+                Button(action: {
+                    print("Copy")
+                }) {
+                    HStack{
+                        Image(systemName: "doc.on.doc")
+                        Text("Copy")
+                    }
+                }
+                Button(action: {
+                    print("Share")
+                }) {
+                    HStack {
+                        Image(systemName: "square.and.arrow.up")
+                        Text("Share")
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ContextMenu/ContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuApp.swift
+//  ContextMenu
+//
+//  Created by Ierchenko Anna  on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
